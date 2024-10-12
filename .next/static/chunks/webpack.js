21cdbcdfbbb9983bdb25435b1090db6abc4b7d8e
Andrea Grandi <andrea.grandi@tiscali.it>
2024-10-12 13:46:34 +0200

    update: works titles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8810043adc47a4c"; }
+/******/ 		__webpack_require__.h = function() { return "19fa2e02576b4d45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.8cca31bd37e0f950.hot-update.js
+++ .next/static/webpack/webpack.8cca31bd37e0f950.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c5dee4b02a5dcc34"; }
+/******/ 	__webpack_require__.h = function() { return "19fa2e02576b4d45"; }
 /******/ }();
 /******/ 
 /******/ }
