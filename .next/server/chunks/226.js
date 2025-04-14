71074118a86d335fe65f8997420de0b174e655eb
Andrea Grandi <andrea.grandi@tiscali.it>
2025-04-14 12:26:49 +0200

    update

---
+++ .next/server/chunks/226.js
@@ -0,0 +1,5 @@
+"use strict";exports.id=226,exports.ids=[226],exports.modules={1227:(e,t,s)=>{s.a(e,async(e,a)=>{try{s.d(t,{Z:()=>h});var n=s(4115),i=e([n]);n=(i.then?(await i)():i)[0];let r=n.default.p`
+  text-align: justify;
+  text-indent: 1em;
+  hyphens: auto;
+`,h=r;a()}catch(e){a(e)}})},8849:(e,t,s)=>{s.a(e,async(e,a)=>{try{s.d(t,{Dx:()=>Title,h_:()=>Meta});var n=s(997),i=s(1664),r=s.n(i),h=s(2210),c=s(6134),l=e([h,c]);[h,c]=l.then?(await l)():l;let Title=({children:e})=>(0,n.jsxs)(h.Box,{children:[n.jsx(h.Link,{as:r(),href:"/works",children:"Works"}),(0,n.jsxs)("span",{children:[" ",n.jsx(c.ChevronRightIcon,{})," "]}),n.jsx(h.Heading,{display:"inline-block",as:"h3",fontSize:20,mb:4,children:e})]}),Meta=({children:e})=>n.jsx(h.Badge,{colorScheme:"green",mr:2,children:e});a()}catch(e){a(e)}})}};
